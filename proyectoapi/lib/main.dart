28afdec95a3b57fdb2b5d29b4647f3fab54c602d
SveenGmz <oswaldogomezu@gmail.com>
2023-12-05 22:44:06 -0600

    abrir imagen en grande

--- proyectoapi/lib/main.dart
+++ proyectoapi/lib/main.dart
@@ -17,8 +17,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var urlData;
   void getApiData()async{
-
-    var url = Uri.parse("https://api.unsplash.com/photos/?client_id=2b4S3Hur3_ojyYrhF1BRjFylYFrME3mMkV5pa4cTNvs");
+    var url = Uri.parse("https://api.unsplash.com/"+
+    "photos/?per_page=30&client_id=2b4S3Hur3_ojyYrhF1BRjFylYFrME3mMkV5pa4cTNvs");
     final res = await http.get(url);
     setState(() {
       urlData = jsonDecode(res.body);
@@ -41,18 +41,50 @@ class _MyAppState extends State<MyApp> {
       body: Center(child: urlData==null
       ? CircularProgressIndicator() 
       : GridView.builder(
-        itemCount: 10,
+        itemCount: 30,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(mainAxisSpacing: 6,crossAxisCount: 2, crossAxisSpacing: 6),
       itemBuilder: (context, i){
-        return Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(urlData[i]['urls']['full'])
-            )
+        return InkWell(
+          onTap: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => FullImageView(
+                  url:urlData[i]['urls']['full'],
+                )));
+            
+          },
+          child: Hero(
+            tag: 'full',
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(urlData[i]['urls']['full'])
+                )
+              ),
+            ),
           ),
         );
       }),),
     );
+  }
+}
+
+class FullImageView extends StatelessWidget {
+  var url;
+  FullImageView({this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag:'full',
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(url),fit: BoxFit.cover
+          )
+        ),
+      ));
   }
 }
 
