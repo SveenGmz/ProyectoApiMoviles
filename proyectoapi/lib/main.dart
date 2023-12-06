8bd8e7828233942fc5aa8b457fb1437bc0b0c855
SveenGmz <oswaldogomezu@gmail.com>
2023-12-05 22:17:21 -0600

    Limitacion de imagenes

--- proyectoapi/lib/main.dart
+++ proyectoapi/lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var urlData;
   void getApiData()async{
+
     var url = Uri.parse("https://api.unsplash.com/photos/?client_id=2b4S3Hur3_ojyYrhF1BRjFylYFrME3mMkV5pa4cTNvs");
     final res = await http.get(url);
     setState(() {
@@ -37,13 +38,16 @@ class _MyAppState extends State<MyApp> {
       appBar: AppBar(
         title: Text('ProyectoApi_IramGmz'),
       ),
-      body: Center(child: urlData==null?CircularProgressIndicator(): GridView.builder(gridDelegate:
-      SliverGridDelegateWithFixedCrossAxisCount(mainAxisSpacing: 6,crossAxisCount: 2, crossAxisSpacing: 6),
+      body: Center(child: urlData==null
+      ? CircularProgressIndicator() 
+      : GridView.builder(
+        itemCount: 10,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(mainAxisSpacing: 6,crossAxisCount: 2, crossAxisSpacing: 6),
       itemBuilder: (context, i){
         return Container(
           decoration: BoxDecoration(
             image: DecorationImage(
-              image: NetworkImage(urlData['urls'][i]['full'])
+              image: NetworkImage(urlData[i]['urls']['full'])
             )
           ),
         );
